59cafdfd5d48a723487fc3240c96be5f6d269f22
tokinohana <oozoratiga@gmail.com>
2023-05-19 11:48:27 +0900

    TEST

---
+++ Assets/New Folder/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;// 玉のオブジェクト
+    private Vector3 offset;//玉からカメラまでの距離
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/New Folder/PiayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PiayerController : MonoBehaviour
+{
+    public float speed; // 動く速さ
+
+    private Rigidbody rb; // Rididbody
+    void Start()
+    {
+        // Rigidbody を取得
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        // カーソルキーの入力を取得
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        // カーソルキーの入力に合わせて移動方向を設定
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // Ridigbody　に力を加えて玉を動かす
+        rb.AddForce(movement * speed);
+    }
+
+}
